22d9ae28bf153f66de00bececb6a4e0ac62f3e96
alexanderalban <albanalexander@gmail.com>
2021-01-25 10:42:21 -0600

    created appbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import React from 'react';
 import './App.css';
+
+import TopAppBar from './components/TopAppBar'
 
 function App() {
   return (
     <div className="App">
-
+      <TopAppBar />
     </div>
   );
 }
