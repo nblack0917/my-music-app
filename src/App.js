8d6f4f5e84e9077de4783a2d61c6d59879abe01e
nblack0917 <nickablack@gmail.com>
2021-01-25 10:45:23 -0600

    completed login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-
+import React, { useState } from 'react';
 import './App.css';
+import Login from './components/Login'
 
-function App() {
+const App = () => {
+  const [loggedIn, setLogin] = useState(false)
+
+  const handleLogin = () => {
+      setLogin(!loggedIn)
+      console.log("click")
+  }
+
   return (
     <div className="App">
-
+      <Login handleLogin={e => handleLogin(e)}/>
     </div>
   );
+
+  return this.state.loggedIn ? (
+    <TopAppBar />
+    <Dashboard />
+    ) :
+    (
+      <TopAppBar />
+      <Login />
+    )
 }
 
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button'
+
+const Login = (props) => {
+    const [loggedIn, setLogin] = useState("false")
+
+    return (
+        <div>
+            <TextField  placeholder="User Name" />
+            <br /><br />
+            <TextField  placeholder="Password" />
+            <br /><br />
+            <Button variant="contained" color="Primary" onClick={props.handleLogin}>Login</Button>
+        </div>
+    )
+}
+
+export default Login
