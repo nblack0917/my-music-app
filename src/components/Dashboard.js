e5ab48f37208e9b6cd3f887f16adad52c7e2343f
alexanderalban <albanalexander@gmail.com>
2021-01-25 12:59:55 -0600

    fixed save dot

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,26 +1,14 @@
-<<<<<<< HEAD
-import React from 'react'
-=======
 import React from 'react';
 import MasterVolume from './MasterVolume';
 import SoundQuality from './SoundQuality';
->>>>>>> alexbranch
 
 function Dashboard() {
     return (
         <div>
-<<<<<<< HEAD
-            <h1>Hello There. add some stuff</h1>
-=======
             <MasterVolume />
             <SoundQuality />
->>>>>>> alexbranch
         </div>
     )
 }
 
 export default Dashboard
-<<<<<<< HEAD
-
-=======
->>>>>>> alexbranch
