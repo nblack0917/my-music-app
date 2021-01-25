1fc0ca30986480b79098acbbe32c39593de46181
nblack0917 <nickablack@gmail.com>
2021-01-25 12:44:56 -0600

    added css style to cards

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,8 +3,12 @@ import OnlineMode from './OnlineMode'
 
 function Dashboard() {
     return (
-        <div>
-            <OnlineMode />
+        <div className="dashboardContainer">
+            <h2 style={{color: "gray", fontSize: "32px"}}>Welcome User!</h2>
+            <div className="dashboardCards">
+                <OnlineMode />
+            </div>
+            <h3 style={{ fontWeight: "800", fontSize: "24px"}}>Sytem Notifications:</h3>
         </div>
     )
 }
