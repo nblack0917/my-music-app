83da76a27b783f205546cbd7881e5ad88cbd97a8
nblack0917 <nickablack@gmail.com>
2021-01-26 11:10:52 -0600

    finished

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,25 +5,41 @@ import Button from '@material-ui/core/Button'
 const Login = (props) => {
     const [userName, setUserName] = useState("");
     const [password, setPassword] = useState("");
+    const [hasName, setHasName] = useState(false)
+    const [hasPass, setHasPass] = useState(false)
 
     const handleUserChange = (e) => {
         setUserName(e.target.value)
+        if(hasName) {
+            setHasName(!hasName)
+        }
     }
+    
     const handlePassChange = (e) => {
         setPassword(e.target.value)
+        if(hasPass) {
+            setHasPass(!hasPass)
+        }
     }
 
     const checkLogin = () => {
-        if(userName, password) {
+        if(!userName || ! password) {
+            if(!userName) {
+                setHasName(!hasName)
+            }
+            if(!password) {
+                setHasPass(!hasPass)
+            }
+        }else if(userName && password) {
             props.handleLogin(userName)
         }
     }
 
     return (
         <div className="loginContainer" style={{ textAlign:"center" }}>
-            <TextField required id="userName" value={userName} onChange={handleUserChange} placeholder="User Name *" />
+            <TextField required error={hasName} id="userName" value={userName} onChange={handleUserChange} placeholder="User Name *" />
             <br /><br />
-            <TextField required id="password" value={password} onChange={handlePassChange} placeholder="Password *" />
+            <TextField required error={hasPass} id="password" value={password} onChange={handlePassChange} placeholder="Password *" />
             <br /><br />
             <Button variant="contained" color="Primary" onClick={checkLogin}>Login</Button>
         </div>
